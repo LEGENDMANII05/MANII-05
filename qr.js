86a020cb1c1b9cd34e6d342a369a4c8529d0320a
LEGENDMANII05 <manimdbot05@gmail.com>
2026-03-03 11:15:25 +0500

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,116 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Arslan_Tech,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function MANII_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_Mani_Tech = Mani_Tech({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_Mani_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Mani_Tech.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_Arslan_Tech.sendMessage(Qr_Code_By_Mani_Tech.user.id, { text: 'MANII-05-MD~' + b64data });
+	
+				   let Arslan_MD_TEXT = `
+╔════════════════════◇
+║『 SESSION CONNECTED』
+║ ✨MANII-05-MD🔷
+║ ✨LEGENDMANII05🔷
+╚════════════════════╝
+
+
+---
+
+╔════════════════════◇
+║『 YOU'VE CHOSEN MANII-05-MD 』
+║ -Set the session ID in Heroku:
+║ - SESSION_ID: 
+╚════════════════════╝
+╔════════════════════◇
+║ 『••• _V𝗶𝘀𝗶𝘁 𝗙𝗼𝗿_H𝗲𝗹𝗽 •••』
+║❍ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞: youtube.com/@powerbymani05
+║❍ 𝐎𝐰𝐧𝐞𝐫 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦: t.me/mani05here 
+║❍ 𝐑𝐞𝐩𝐨: https://github.com/LEGENDMANII05/MANII-05-MD 
+║❍ 𝐖𝐚𝐆𝗿𝐨𝐮𝐩: Coming...
+║❍ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: https://whatsapp.com/channel/0029VbDXEsMKrWQyjdNioe1H
+║❍ 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦: _https://www.instagram.com/rahman.mani05 
+║ 
+╚═════════════════════╝
+
+
+---
+
+Don't Forget To Give Star⭐ To My Repo
+______________________________`;
+	 await Qr_Code_By_Mani_Tech.sendMessage(Qr_Code_By_Mani_Tech.user.id,{text:Mani_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_Mani_Tech.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					MANII_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await MANII_MD_QR_CODE()
+});
+module.exports = router
